3e5a2994b5c4d8cf03fed6cd6e14449087c13539
viniciusfinger <fingervinicius@icloud.com>
2024-09-22 21:33:12 -0300

    starting v3

---
+++ travelAgent_v3.py
@@ -0,0 +1,4 @@
+# Recriar tudo do zero
+# Separar eventos e locais
+# Usar prompt templates
+# Não precisa ser nesse modelo, mas seria interessante fazer um fine tuning. tem no canal inteligencia mil grau
