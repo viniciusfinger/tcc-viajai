a0a0262cdbd6ac28ca3d05466a17a9ce77bd76b4
viniciusfinger <fingervinicius@icloud.com>
2025-03-06 22:53:16 -0300

    Initializing itinerary creation screen

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'create',
+        pathMatch: 'full'
+    },
+    {
+        path: 'create',
+        loadComponent: () => import('./pages/create-itinerary/create-itinerary.component')
+            .then(m => m.CreateItineraryComponent)
+    }
+];
+
