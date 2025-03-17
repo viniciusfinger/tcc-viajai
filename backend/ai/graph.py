df7b205c2d1a9ab9b85cd97365359c416f144a81
viniciusfinger <fingervinicius@icloud.com>
2025-03-17 14:49:01 -0300

    Parallelize nodes

--- backend/ai/graph.py
+++ backend/ai/graph.py
@@ -8,15 +8,14 @@ from backend.ai.node.touristic_attractions_agent import touristic_attractions_ag
 def create_graph() -> CompiledStateGraph:
     graph = StateGraph(State)
     
-    #todo: paralelizar os nodes para ganhar performance
     graph.add_node("events_fetcher_agent", events_fetcher_agent)
     graph.add_node("touristic_attractions_agent", touristic_attractions_agent)
     
     graph.add_edge(START, "events_fetcher_agent")
-    graph.add_edge("events_fetcher_agent", "touristic_attractions_agent")
+    graph.add_edge(START, "touristic_attractions_agent")
+    graph.add_edge("events_fetcher_agent", END)
+    graph.add_edge("touristic_attractions_agent", END)
     
     # todo: adicionar node de criação de itinerário
-    
-    graph.add_edge("touristic_attractions_agent", END)
     
     return graph.compile()
