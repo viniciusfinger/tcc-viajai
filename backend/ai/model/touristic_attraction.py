6c93c22c4379620ebd8ada89593bdee512d9d16b
viniciusfinger <fingervinicius@icloud.com>
2025-03-16 22:53:12 -0300

    Use output parsers to parse data

--- backend/ai/model/touristic_attraction.py
+++ backend/ai/model/touristic_attraction.py
@@ -1,4 +1,6 @@
-class TouristicAttraction:
-    name: str
-    description: str
-    address: str
+from pydantic import BaseModel, Field
+
+class TouristicAttraction(BaseModel):
+    name: str = Field(description="The name of the touristic attraction")
+    description: str = Field(description="A brief description of the touristic attraction")
+    address: str = Field(description="The address of the touristic attraction")
