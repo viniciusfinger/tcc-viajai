b385db9e99f4c135e8135f0d12099c20149c3e34
viniciusfinger <fingervinicius@icloud.com>
2025-03-27 20:01:55 -0300

    Change date to str to accept periods

--- backend/ai/model/event.py
+++ backend/ai/model/event.py
@@ -1,4 +1,3 @@
-from datetime import date as date_type
 from pydantic import BaseModel, Field
 
 
@@ -6,6 +5,6 @@ class Event(BaseModel):
     name: str = Field(description="The name of the event")
     description: str = Field(description="The description of the event")
     address: str | None = Field(default=None, description="The address of the event, let empty if it is not available")
-    date: date_type | None = Field(default=None, description="The date of the event, let empty if it is not available")
-    time: str | None = Field(default=None, description="The time of the event, let empty if it is not available")
+    date: str | None = Field(default=None, description="The date of the event in format dd/mm/yyyy, let empty if it is not available")
+    time: str | None = Field(default=None, description="The time of the event in format hh:mm 24h, let empty if it is not available")
     source: str | None = Field(default=None, description="URL of the event source, let empty if it is not available")
