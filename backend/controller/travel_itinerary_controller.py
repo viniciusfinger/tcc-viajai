67de439ee09218ce71a3b50cfad356c9328698e6
viniciusfinger <fingervinicius@icloud.com>
2025-04-05 21:28:52 -0300

    Handle errors

--- backend/controller/travel_itinerary_controller.py
+++ backend/controller/travel_itinerary_controller.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter
-from backend.ai.model.travel_itinerary import TravelItinerary
+from fastapi import APIRouter, HTTPException
 from backend.model.TravelInput import TravelInput
 import backend.service.ai_service as ai_service
 import logging
@@ -10,11 +9,15 @@ travel_itinerary_router = APIRouter(prefix="/travel-itineraries")
 
 @travel_itinerary_router.post("")
 async def get_travel_itinerary(travel_input: TravelInput):
-    travel_input.trace_id = str(uuid.uuid4())
-    logging.info(f"[Travel Itinerary Controller] Generating travel itinerary. Trace: {travel_input.trace_id}")
-    
-    response = ai_service.invoke(travel_input)
-    
-    logging.info(f"[Travel Itinerary Controller] 200 OK - Success generating travel itinerary. Trace: {travel_input.trace_id}")
-    
-    return response
+    try:
+        travel_input.trace_id = str(uuid.uuid4())
+        logging.info(f"[Travel Itinerary Controller] Generating travel itinerary. Trace: {travel_input.trace_id}")
+        
+        response = ai_service.invoke(travel_input)
+        
+        logging.info(f"[Travel Itinerary Controller] 200 OK - Success generating travel itinerary. Trace: {travel_input.trace_id}")
+        
+        return response
+    except Exception as e:
+        logging.error(f"[Travel Itinerary Controller] Error generating travel itinerary. Trace: {travel_input.trace_id}. Error: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal server error: " + str(e))
