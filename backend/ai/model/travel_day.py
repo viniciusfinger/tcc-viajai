eac1a48ff2dd0a19e3f4dc4792994f07844d94b2
viniciusfinger <fingervinicius@icloud.com>
2025-04-06 20:30:54 -0300

    Fix activities name

--- backend/ai/model/travel_day.py
+++ backend/ai/model/travel_day.py
@@ -3,4 +3,4 @@ from backend.ai.model.activity import Activity
 
 class TravelDay(BaseModel):
     date: str = Field(description="The date of the day")
-    activies: list[Activity] = Field(description="The list of activities of the day")
+    activities: list[Activity] = Field(description="The list of activities of the day")
--- frontend/src/app/commons/travel-day.ts
+++ frontend/src/app/commons/travel-day.ts
@@ -2,5 +2,5 @@ import { Activity } from "./activity";
 
 export interface TravelDay {
     date: string;
-    activies: Activity[];
+    activities: Activity[];
 }
