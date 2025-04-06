52a4b40741a70ac7d8ce0321b65826cbd314bc9f
viniciusfinger <fingervinicius@icloud.com>
2025-04-06 15:22:24 -0300

    Return travel itinerary instead all graph state

--- backend/controller/travel_itinerary_controller.py
+++ backend/controller/travel_itinerary_controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from backend.model.TravelInput import TravelInput
+from backend.ai.model.travel_itinerary import TravelItinerary
 import backend.service.ai_service as ai_service
 import logging
 import uuid
@@ -8,7 +9,7 @@ import uuid
 travel_itinerary_router = APIRouter(prefix="/travel-itineraries")
 
 @travel_itinerary_router.post("")
-async def get_travel_itinerary(travel_input: TravelInput):
+async def get_travel_itinerary(travel_input: TravelInput) -> TravelItinerary:
     try:
         travel_input.trace_id = str(uuid.uuid4())
         logging.info(f"[Travel Itinerary Controller] Generating travel itinerary. Trace: {travel_input.trace_id}")
@@ -17,7 +18,7 @@ async def get_travel_itinerary(travel_input: TravelInput):
         
         logging.info(f"[Travel Itinerary Controller] 200 OK - Success generating travel itinerary. Trace: {travel_input.trace_id}")
         
-        return response
+        return response["travel_itinerary"]
     except Exception as e:
         logging.error(f"[Travel Itinerary Controller] Error generating travel itinerary. Trace: {travel_input.trace_id}. Error: {str(e)}")
         raise HTTPException(status_code=500, detail="Internal server error: " + str(e))
